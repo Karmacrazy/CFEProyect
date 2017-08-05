f6e3d9405ce81112b585474c2c4318713cde879c
Karmacrazy <apc_george_@hotmail.com>
2017-08-04 23:42:06 -0500

    Add files via upload

---
+++ modalDeleteAsigTrabajo.php
@@ -0,0 +1,15 @@
+<form id="deleteData">
+<div class="modal fade" id="deleteAsigTrabajo" tabindex="-1" role="dialog" aria-labelledby="deleteLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <input type="hidden" id="idb0" name="idb0">
+      <h2 class="text-center text-muted">Estas seguro?</h2>
+	  <p class="lead text-muted text-center" style="display: block;margin:10px">Esta acción eliminará de forma permanente el registro. Deseas continuar?</p>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-lg btn-default" data-dismiss="modal">Cancelar</button>
+        <button type="button" id="btnDelete" class="btn btn-lg btn-primary">Aceptar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
